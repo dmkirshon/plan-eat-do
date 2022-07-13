2af7f3dcc96253933c234571311c3aeca4468d6e
dmkirshon <kirshon.david@gmail.com>
2022-07-13 12:05:51 +0200

    chores: Add firebase config from project creation

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXKvjq2dTtE0DtInmsQm_hfdHbJc9fdA8",
+  authDomain: "plan-eat-do.firebaseapp.com",
+  projectId: "plan-eat-do",
+  storageBucket: "plan-eat-do.appspot.com",
+  messagingSenderId: "971989929608",
+  appId: "1:971989929608:web:01c757856119cdcdd5593d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
